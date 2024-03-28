6f1873610d5652a43cd0fe92d3f0cad363e0e5e1
Your Name <you@example.com>
2024-03-28 21:26:18 +0900

    21:16

---
+++ src/com/diworksdev/webproj/action/WelcomeAction.java
@@ -0,0 +1,10 @@
+package com.diworksdev.webproj.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class WelcomeAction extends ActionSupport {
+    public String execute() {
+        return SUCCESS;
+    }
+}
+
